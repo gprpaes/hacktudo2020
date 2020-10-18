6ea6cfd75b4b85cbda0118f15956deddbedf29f1
Guilherme Paes <guilhermep98@gmail.com>
2020-10-17 22:58:50 -0300

    controllers

--- api/src/produto/produto.controller.ts
+++ api/src/produto/produto.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Put,
+  Post,
+  Get,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { ProdutoService } from './produto.service';
 
 @Controller('produto')
-export class ProdutoController {}
+export class ProdutoController {
+  constructor(private produtoService: ProdutoService) {}
+  @Post('store')
+  create(@Body() dto: Object) {
+    //return this.entregadorService.createEntregador(dto);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    //return this.entregadorService.findOneEntregador(id);
+  }
+
+  @Get()
+  findAll() {
+    //return this.entregadorService.findAll();
+  }
+
+  @Delete(':id')
+  deleteOne(@Param('id') id: number) {
+    //return this.entregadorService.deleteEntregador(id);
+  }
+
+  @Put(':id')
+  updateOne(@Param('id') id: number, @Body() dto: Object) {
+    //return this.entregadorService.updateEntregador(id, dto)
+  }
+}
