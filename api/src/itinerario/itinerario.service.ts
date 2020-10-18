d7b08e90b1ad578aa8e064953696be9121623b1b
Guilherme Paes <guilhermep98@gmail.com>
2020-10-18 01:42:42 -0300

    service itinerario

--- api/src/itinerario/itinerario.service.ts
+++ api/src/itinerario/itinerario.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, UpdateResult } from 'typeorm';
+import { Itinerario } from './itinerario.entity';
+import ItinerarioDTO from './itinerarioDTO';
 
 @Injectable()
-export class ItinerarioService {}
+export class ItinerarioService {
+  constructor(
+    @InjectRepository(Itinerario)
+    private itinerarioRepository: Repository<Itinerario>,
+  ) {}
+
+  async createItinerario(dto: ItinerarioDTO): Promise<Itinerario> {
+    return await this.itinerarioRepository.save(dto);
+  }
+
+  async findOne(id: number): Promise<Itinerario> {
+    return await this.itinerarioRepository.findOne(id);
+  }
+
+  async findAll(): Promise<Itinerario[]> {
+    return await this.itinerarioRepository.find();
+  }
+
+  async updateItinerario(id: number, dto: ItinerarioDTO): Promise<UpdateResult>{
+      return await this.itinerarioRepository.update(id, dto);
+  }
+
+  async deleteItinerario(id: number): Promise<UpdateResult>{
+      return await this.itinerarioRepository.softDelete(id);
+  }
+}
