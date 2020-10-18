0e676697e0487b7d1cf7eb41af442a691f3d3d49
Guilherme Paes <guilhermep98@gmail.com>
2020-10-17 21:56:47 -0300

    service entregador

--- api/src/entregador/entregador.controller.ts
+++ api/src/entregador/entregador.controller.ts
@@ -1,29 +1,39 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { EntregadorService } from './entregador.service';
 
 @Controller('entregador')
 export class EntregadorController {
+  constructor(private entregadorService: EntregadorService) {}
   @Post('store')
   create(@Body() dto: Object) {
     return dto;
   }
 
   @Get(':id')
-  findOne(@Param('id') id: number){
-      return id;
+  findOne(@Param('id') id: number) {
+    return id;
   }
 
   @Get()
-  findAll(){
-      return 'tudão'
+  findAll() {
+    return 'tudão';
   }
 
   @Delete(':id')
-  deleteOne(@Param('id') id: number){
-      return {affected: id};
+  deleteOne(@Param('id') id: number) {
+    return { affected: id };
   }
 
   @Put(':id')
-  updateOne(@Param('id') id: number, @Body() dto:Object){
-      return id;
+  updateOne(@Param('id') id: number, @Body() dto: Object) {
+    return id;
   }
 }
--- api/src/entregador/entregador.service.ts
+++ api/src/entregador/entregador.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { Entregador } from './entregador.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class EntregadorService {}
+export class EntregadorService {
+  constructor(
+    @InjectRepository(Entregador)
+    private entregadorRepository: Repository<Entregador>,
+  ) {}
+
+  async createEntregador(dto: Object): Promise<Entregador> {
+    return await this.entregadorRepository.save(dto);
+  }
+
+  async findOneEntregador(id: number): Promise<Entregador>{
+      return await this.entregadorRepository.findOne(id);
+  }
+
+  async findAll(): Promise<Entregador[]>{
+      return await this.entregadorRepository.find();
+  }
+
+  async updateEntregador(id: number, dto: Object): Promise<UpdateResult>{
+      return await this.entregadorRepository.update(id, dto);
+  }
+
+  async deleteEntregador(id: number): Promise<UpdateResult>{
+      return await this.entregadorRepository.softDelete(id);
+  }
+}
