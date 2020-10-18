b239ea6d088089c131d993ec608c9064a96c7ee0
Guilherme Paes <guilhermep98@gmail.com>
2020-10-17 21:18:18 -0300

    assinaturas controller entregador

--- api/src/entregador/entregador.controller.ts
+++ api/src/entregador/entregador.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('entregador')
-export class EntregadorController {}
+export class EntregadorController {
+  @Post('store')
+  create(@Body() dto: Object) {
+    return dto;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number){
+      return id;
+  }
+
+  @Get()
+  findAll(){
+      return 'tudão'
+  }
+
+  @Delete(':id')
+  deleteOne(@Param('id') id: number){
+      return {affected: id};
+  }
+
+  @Put(':id')
+  updateOne(@Param('id') id: number, @Body() dto:Object){
+      return id;
+  }
+}
